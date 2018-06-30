327649ae5928f9dff45bd63876f01de56a245527
Suzie Neave <suzieneave@dhcp-10-249-77-57.eduroam.wireless.private.cam.ac.uk>
2018-06-30 20:58:23 +0100

    deleted a lot

--- 4_experiments.py
+++ 4_experiments.py
@@ -5,8 +5,10 @@ Created on Sat Jun 30 16:57:51 2018
 
 @author: suzieneave
 """
-   
-print("Here", end=" ")
-print("here")
-print(\a)
-input("\n\nPress enter to exit")
+
+import matplotlib
+import numpy
+import random
+
+
+
