f0ca1f583d319de6169ecc332796e51416e047bb
Suzie Neave <suzieneave@dhcp-10-249-77-57.eduroam.wireless.private.cam.ac.uk>
2018-06-30 17:05:06 +0100

    added new file for experimenting and gave file 3 .py ending

---
+++ 4_experiments.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 30 16:57:51 2018
+
+@author: suzieneave
+"""
+
+print("Here", end=" ")
+print("here")
+input("\n\nPress enter to exit")
