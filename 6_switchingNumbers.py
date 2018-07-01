535d666031a1c5b5065456211f7833aa7791b69a
Suzie Neave <suzieneave@dhcp-10-249-77-57.eduroam.wireless.private.cam.ac.uk>
2018-07-01 12:40:35 +0100

    added switching numbers prog

---
+++ 6_switchingNumbers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  1 12:36:16 2018
+
+@author: suzieneave
+"""
+choice = input()
+if choice == "a":
+    print("b")
+elif choice == "b":
+    print("a")
+
+input("Press enter to exit.")
